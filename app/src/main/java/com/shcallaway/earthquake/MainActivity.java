0f41fffe537b7b72494e12ec07ba7c00bc2b4715
Sherwood Callaway <shcallaway@gmail.com>
2016-11-28 14:26:41 -0500

    Added ListView to MainActivity

--- app/src/main/java/com/shcallaway/earthquake/MainActivity.java
+++ app/src/main/java/com/shcallaway/earthquake/MainActivity.java
@@ -2,12 +2,29 @@ package com.shcallaway.earthquake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> quakes = new ArrayList<>();
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quakes.add("Hello world");
+        quakes.add("I am a quake");
+
+        // Assign ListView to the listView object in layout.
+        listView = (ListView) findViewById(R.id.listView);
+
+        // Create an ArrayAdapter from quakes Array and set it to listView.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, quakes);
+        listView.setAdapter(adapter);
     }
 }
